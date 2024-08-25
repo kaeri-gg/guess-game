4167cb7c6e3b847d5879c70e8cc427be29592e0a
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2024-08-26 05:35:09 +0800

    rename functions, added animations, fix new session

--- src/js/main.js
+++ src/js/main.js
@@ -8,11 +8,12 @@ export class Session {
     this.countdownMainDiv = $('#countdownMainDiv');
     this.welcomeMainDiv = $('#welcomeMainDiv');
     this.startGameMainDiv = $('#startGameMainDiv');
-    this.startButton = $('#gameStart');
+    this.startButton = $('#startButton');
     this.counterDiv = $('#counterText');
-    this.startNewDiv = $('#startNewDiv');
+    this.newGameButtonsDiv = $('#newGameButtonsDiv');
     this.startNew = $('#startNew');
     this.reset = $('#reset');
+    this.modes = $('input[name="mode"]');
 
     this.playerNameInput = $('#playerNameInput');
     this.playerNameText = $('#playerNameText');
@@ -30,34 +31,31 @@ export class Session {
     this.hardModeOption = $('#hardMode');
 
     this.game = new Game();
-    this.counter = 3;
-    this.startGame = null;
 
-    this.youWonDiv.hide();
-    this.hintDiv.hide();
+    this.showWelcomePage();
+    this.subscribeEventListeners();
+    this.resetFields();
+  }
 
-    this.initialize();
+  subscribeEventListeners() {
+    this.startButton.on('click', () => {
+      this.showCountDownPage();
+
+      this.checkMode();
+      this.timerId = setInterval(() => this.countDown(), 1000);
+    });
 
     this.startNew.on('click', () => {
       console.log('start new click');
-      //this.new();
       this.showWelcomePage();
     });
 
     this.reset.on('click', () => {
       console.log('reset click');
-      this.start();
     });
-  }
 
-  initialize() {
-    this.showWelcomePage();
-    this.startButton.on('click', () => {
-      this.showCountDownPage();
-
-      this.checkMode();
-      const delay = 1000;
-      this.startGame = setInterval(() => this.timer(), delay);
+    this.playerSubmitButton.on('click', () => {
+      this.guess();
     });
   }
 
@@ -67,29 +65,28 @@ export class Session {
     console.log('Selected mode:', this.selectedMode);
   }
 
-  timer() {
-    if (--this.counter === 0) {
-      this.showNewSession();
-      clearInterval(this.showNewSession);
-      return;
-    }
+  countDown() {
+    this.counter--;
+    console.log(this.counter);
     this.counterDiv.text(this.counter);
+
+    if (this.counter <= 0) {
+      this.showNewSession();
+    }
   }
 
   // show and start new session
   showNewSession() {
+    clearInterval(this.timerId);
     this.showStartGamePage();
-    this.playerSubmitDiv.removeClass('hidden').addClass('flex');
-    this.startNewDiv.addClass('hidden');
-    this.start();
+    this.showGameDetails();
   }
 
   showWelcomePage() {
     this.countdownMainDiv.hide();
     this.startGameMainDiv.hide();
     this.welcomeMainDiv.show(); // display
-
-    // reset
+    this.resetFields();
   }
 
   showCountDownPage() {
@@ -101,35 +98,45 @@ export class Session {
   showStartGamePage() {
     this.welcomeMainDiv.hide();
     this.countdownMainDiv.hide();
+    this.newGameButtonsDiv.hide();
+
     this.startGameMainDiv.show();
+    this.playerSubmitDiv.show();
   }
 
-  start() {
-    // TODO: display timer
-    // TODO: display player name
+  resetFields() {
+    this.counter = 3;
 
+    this.modes.prop('checked', false); // clear the radio button
+    this.playerNameInput.val(''); // clear the radio button
+    this.playerInput.val(''); //reset player input in start game page
+    this.hintDiv.hide();
+    this.youWonDiv.hide();
+    this.newGameButtonsDiv.hide();
+    this.playerSubmitDiv.show('slow');
+    this.startButton.show('slow');
+  }
+
+  showGameDetails() {
     const mode = this.selectedMode;
     const formattedMode = mode + ' [' + this.game[mode][0] + ', ' + this.game[mode][1] + ']';
 
     this.playerNameText.text(this.playerNameInput.val());
     this.modeText.text(formattedMode);
-
-    this.playerSubmitButton.on('click', () => {
-      this.guess();
-    });
   }
 
   guess() {
     // TODO: get the player name
     const inputValue = parseInt($('#playerInput').val());
+    this.hintDiv.hide();
 
     if (this.game.isGreaterThan(inputValue)) {
-      this.hintDiv.show();
+      this.hintDiv.fadeIn();
       this.hintText.text('lower!');
     }
 
     if (this.game.isLessThan(inputValue)) {
-      this.hintDiv.show();
+      this.hintDiv.fadeIn();
       this.hintText.text('higher!');
     }
 
@@ -137,7 +144,7 @@ export class Session {
       this.youWonDiv.show();
       this.hintDiv.hide();
       this.playerSubmitDiv.hide();
-      this.startNewDiv.show();
+      this.newGameButtonsDiv.show('fast');
     }
   }
 }
