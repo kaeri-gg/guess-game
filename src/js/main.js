f76d710a5feeb3c1325c9977aa875bcc93e62129
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2024-08-26 03:33:03 +0800

    added functions for new session or reset game

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,14 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export class Session {
   constructor() {
-    this.countdownDiv = $('#countdownMainDiv');
-    this.welcomeDiv = $('#welcomeMainDiv');
-    this.startGameDiv = $('#startGameMainDiv');
+    this.countdownMainDiv = $('#countdownMainDiv');
+    this.welcomeMainDiv = $('#welcomeMainDiv');
+    this.startGameMainDiv = $('#startGameMainDiv');
     this.startButton = $('#gameStart');
-    this.counterDiv = $('#counter');
+    this.counterDiv = $('#counterText');
     this.startNewDiv = $('#startNewDiv');
+    this.startNew = $('#startNew');
+    this.reset = $('#reset');
 
     this.playerNameInput = $('#playerNameInput');
     this.playerNameText = $('#playerNameText');
@@ -35,18 +37,32 @@ export class Session {
     this.hintDiv.hide();
 
     this.initialize();
+
+    this.startNew.on('click', () => {
+      console.log('start new click');
+      //this.new();
+      this.showWelcomePage();
+    });
+
+    this.reset.on('click', () => {
+      console.log('reset click');
+      this.start();
+    });
   }
 
   initialize() {
-    this.startButton.click(() => {
-      // TODO : .show / .hide
-      this.welcomeDiv.addClass('hidden');
-      this.countdownDiv.removeClass('hidden').addClass('flex');
+    this.showWelcomePage();
+    this.startButton.on('click', () => {
+      this.showCountDownPage();
 
       this.checkMode();
       const delay = 1000;
       this.startGame = setInterval(() => this.timer(), delay);
     });
+  }
+
+  new() {
+    // TODO : .show / .hide
   }
 
   checkMode() {
@@ -66,11 +82,30 @@ export class Session {
 
   // show and start new session
   showNewSession() {
-    this.countdownDiv.removeClass('flex').addClass('hidden');
-    this.startGameDiv.removeClass('hidden').addClass('flex');
+    this.showStartGamePage();
     this.playerSubmitDiv.removeClass('hidden').addClass('flex');
     this.startNewDiv.addClass('hidden');
     this.start();
+  }
+
+  showWelcomePage() {
+    this.countdownMainDiv.hide();
+    this.startGameMainDiv.hide();
+    this.welcomeMainDiv.show(); // display
+
+    // reset
+  }
+
+  showCountDownPage() {
+    this.welcomeMainDiv.hide();
+    this.startGameMainDiv.hide();
+    this.countdownMainDiv.show();
+  }
+
+  showStartGamePage() {
+    this.welcomeMainDiv.hide();
+    this.countdownMainDiv.hide();
+    this.startGameMainDiv.show();
   }
 
   start() {
@@ -92,13 +127,17 @@ export class Session {
     // TODO: get the player name
     const inputValue = parseInt($('#playerInput').val());
 
-    if (this.game.isGreaterThan(inputValue) === true) {
-      this.hintDiv.show();
-      this.hintText.text('higher!');
-    } else if (this.game.isLessThan(inputValue) === true) {
+    if (this.game.isGreaterThan(inputValue)) {
       this.hintDiv.show();
       this.hintText.text('lower!');
-    } else {
+    }
+
+    if (this.game.isLessThan(inputValue)) {
+      this.hintDiv.show();
+      this.hintText.text('higher!');
+    }
+
+    if (this.game.isEqualTo(inputValue)) {
       this.youWonDiv.show();
       this.hintDiv.hide();
       this.playerSubmitDiv.hide();
