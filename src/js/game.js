80803f5d95d07a718b33c47db1f0b6c673222ce5
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2024-08-30 03:36:21 +0800

    added timer

--- src/js/game.js
+++ src/js/game.js
@@ -1,43 +1,57 @@
 import { generator } from './generator';
 
 export class Game {
-    constructor() {
-        this.modes = {
-            Easy: [1, 10],
-            Normal: [1, 100],
-            Hard: [1, 1000],
-        };
-    }
+  constructor() {
+    this.modes = {
+      Easy: [1, 10],
+      Normal: [1, 100],
+      Hard: [1, 1000],
+    };
+    this.startedAt = +Infinity;
+    this.stoppedAt = +Infinity;
+  }
 
-    setMode(mode) {
-        this.currentMode = mode;
+  setMode(mode) {
+    this.currentMode = mode;
 
-        const selectedMode = this.modes[mode];
+    const selectedMode = this.modes[mode];
 
-        if (!selectedMode) throw new Error(`Mode "${mode}" doesn't exists.`);
+    if (!selectedMode) throw new Error(`Mode "${mode}" doesn't exists.`);
 
-        this.numberToGuess = generator.generateNumber(...selectedMode);
-    }
+    this.numberToGuess = generator.generateNumber(...selectedMode);
+  }
 
-    getFormattedMode() {
-        const [min, max] = this.modes[this.currentMode];
+  getFormattedMode() {
+    const [min, max] = this.modes[this.currentMode];
 
-        return `${this.currentMode} [${min}, ${max}]`;
-    }
+    return `${this.currentMode} <b>[${min}, ${max}]</b>`;
+  }
 
-    isEqualTo(value) {
-        return value === this.numberToGuess;
-    }
+  isEqualTo(value) {
+    return value === this.numberToGuess;
+  }
 
-    isGreaterThan(value) {
-        return this.numberToGuess > value;
-    }
+  isGreaterThan(value) {
+    return this.numberToGuess > value;
+  }
 
-    isLessThan(value) {
-        return this.numberToGuess < value;
-    }
+  isLessThan(value) {
+    return this.numberToGuess < value;
+  }
 
-    reset() {
-        this.setMode(this.currentMode);
-    }
+  reset() {
+    this.setMode(this.currentMode);
+  }
+
+  startTimer() {
+    this.startedAt = Date.now();
+  }
+
+  stopTimer() {
+    this.stoppedAt = Date.now();
+  }
+
+  getPlayerTime() {
+    return this.stoppedAt - this.startedAt;
+  }
 }
